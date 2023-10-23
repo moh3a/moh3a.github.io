90d37b7e0c53d3ab7288d899314631b2e5209557
moh3a <aitabdelmalekma@gmail.com>
2023-10-23 21:15:59 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# reglini-dz Blog
---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_NAME = "reglini-dz Blog";
+export const SITE_TITLE = `Code Here | ${SITE_NAME}`;
+export const SITE_DESCRIPTION = "Discover programming.";
