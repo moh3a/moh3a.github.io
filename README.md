4852d33b2f85a0d904b72ad8b39febe3e1fbcb44
moh3a <aitabdelmalekma@gmail.com>
2023-10-24 21:22:50 +0100

    Setup tailwind

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # reglini-dz Blog
+
+## todo
+
+- add fetching blog server side
+- slugify title and set max length and ensure unique slug
+- add page: `explore topics` for tags
+- add dark theme
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_NAME = "reglini-dz Blog";
 export const SITE_TITLE = `Code Here | ${SITE_NAME}`;
 export const SITE_DESCRIPTION = "Discover programming.";
+export const WRITE_URL = "https://write.reglini-dz.com";
