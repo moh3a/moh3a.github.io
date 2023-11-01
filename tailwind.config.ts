30cf3ef6ffeebeaf1b6e8c47aaea15531002b96f
moh3a <aitabdelmalekma@gmail.com>
2023-11-01 19:30:26 +0100

    Publish as a github page 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -14,4 +14,4 @@ export default {
     },
   },
   plugins: [],
-};
+} satisfies Config;
